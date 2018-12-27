06c336c7450099f7a99662c4039ba2e04817e64d
Mohamed Meabed <mo.meabed@gmail.com>
2018-12-27 14:57:23 +0400

    Update

--- src/form/index.js
+++ src/form/index.js
@@ -1,28 +1,33 @@
 const defaults = require("lodash.defaults");
 const isFunction = require("lodash.isfunction");
+import merge from "merge-deep";
 
-function defaultOptions() {
+function defaultOptions(config) {
+
   return {
     outline: "0",
     borderRadius: ".25rem",
-    padding: "1rem"
+    padding: "1rem",
+    input: {
+      backgroundColor: config("colors.grey-lightest"),
+      color: config("colors.black")
+    }
   };
 }
 
-export const form = function (options) {
-  options = isFunction(options)
-    ? options(defaultOptions())
-    : defaults(options, defaultOptions());
+export const form = function (params = { form: {}, input: {} }) {
 
   return function ({ addComponents, config }) {
+    const options = merge(defaultOptions(config), params);
+    
     addComponents([
       {
         ".form-input": {
           outline: options.outline,
           borderRadius: options.borderRadius,
           padding: options.padding,
-          backgroundColor: config("colors.secondary-lighter"),
-          color: config("colors.black")
+          backgroundColor: options.input.backgroundColor,
+          color: options.input.color
         }
       }
     ]);
